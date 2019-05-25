d7693cf78f8608a1735ea2dfeeb3463ae0940a8a
王灼洲 <wangzhuozhou@sensorsdata.cn>
2019-05-25 11:23:19 +0800

    modify

--- SensorsSDK/SensorsAnalyticsSDK.m
+++ SensorsSDK/SensorsAnalyticsSDK.m
@@ -6,7 +6,15 @@
 //  Copyright © 2019 王灼洲. All rights reserved.
 //
 
+#include <sys/sysctl.h>
 #import "SensorsAnalyticsSDK.h"
+
+#define VERSION @"1.0.0"
+
+@interface SensorsAnalyticsSDK()
+@property (nonatomic, strong) NSDictionary *automaticProperties;
+@property (nonatomic, assign) BOOL applicationWillResignActive;
+@end
 
 @implementation SensorsAnalyticsSDK
 
@@ -14,7 +22,7 @@
     static SensorsAnalyticsSDK *sharedInstance = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        NSLog(@"init");
+        NSLog(@"initSensorsSDK");
         sharedInstance = [[SensorsAnalyticsSDK alloc] initSensorsSDK];
     });
     return sharedInstance;
@@ -23,23 +31,55 @@
 - (instancetype)initSensorsSDK {
     self = [super init];
     if (self) {
+        _applicationWillResignActive = NO;
+        self.automaticProperties = [self collectAutomaticProperties];
         [self setUpListeners];
     }
     return self;
 }
 
 - (void)setUpListeners {
-    // 监听 App 启动事件
     NSNotificationCenter *notificationCenter = [NSNotificationCenter defaultCenter];
     
+    //监听 UIApplicationDidBecomeActiveNotification 本地通知
     [notificationCenter addObserver:self
                            selector:@selector(applicationDidBecomeActive:)
                                name:UIApplicationDidBecomeActiveNotification
                              object:nil];
+    
+    //监听 applicationWillResignActive 本地通知
+    [notificationCenter addObserver:self
+                           selector:@selector(applicationWillResignActive:)
+                               name:UIApplicationWillResignActiveNotification
+                             object:nil];
+    
+    //监听 applicationDidEnterBackground 本地通知
+    [notificationCenter addObserver:self
+                           selector:@selector(applicationDidEnterBackground:)
+                               name:UIApplicationDidEnterBackgroundNotification
+                             object:nil];
 }
 
+//触发 $AppStart 事件
 - (void)applicationDidBecomeActive:(NSNotification *)notification {
+    NSLog(@"applicationDidBecomeActive");
+    if (_applicationWillResignActive) {
+        _applicationWillResignActive = NO;
+        return;
+    }
     [self track:@"$AppStart" andProperties:nil];
+}
+
+- (void)applicationWillResignActive:(NSNotification *)notification {
+    NSLog(@"applicationWillResignActive");
+    _applicationWillResignActive = YES;
+}
+
+//触发 $AppEnd 事件
+- (void)applicationDidEnterBackground:(NSNotification *)notification {
+    NSLog(@"applicationDidEnterBackground");
+    _applicationWillResignActive = NO;
+    [self track:@"$AppEnd" andProperties:nil];
 }
 
 - (void)track:(NSString *)eventName andProperties:(NSDictionary *)properties {
@@ -54,8 +94,7 @@
     
     NSMutableDictionary *libProperties = [[NSMutableDictionary alloc] init];
     
-    //$os
-    [libProperties setObject:@"iOS" forKey:@"$os"];
+    [libProperties addEntriesFromDictionary:self.automaticProperties];
     
     //properties
     if (properties) {
@@ -71,6 +110,30 @@
 
 + (UInt64)getCurrentTime {
     return [[NSDate date] timeIntervalSince1970] * 1000;
+}
+
+- (NSDictionary *)collectAutomaticProperties {
+    NSMutableDictionary *p = [NSMutableDictionary dictionary];
+    [p setObject:@"iOS" forKey:@"$os"];
+    [p setObject:@"iOS" forKey:@"$lib"];
+    [p setObject:@"Apple" forKey:@"$manufacturer"];
+    [p setObject:VERSION forKey:@"$lib_version"];
+    [p setValue:[[NSBundle mainBundle] infoDictionary][@"CFBundleShortVersionString"] forKey:@"$app_version"];
+    
+    UIDevice *device = [UIDevice currentDevice];
+    [p setObject:[device systemVersion] forKey:@"os_version"];
+    
+    [p setObject:[self deviceModel] forKey:@"$model"];
+    return [p copy];
+}
+
+- (NSString *)deviceModel {
+    size_t size;
+    sysctlbyname("hw.machine", NULL, &size, NULL, 0);
+    char answer[size];
+    sysctlbyname("hw.machine", answer, &size, NULL, 0);
+    NSString *results = @(answer);
+    return results;
 }
 
 @end
