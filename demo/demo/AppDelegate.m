3484ef820ae587c64ffa2f3b7c2e81bdf7eed1fe
王灼洲 <wangzhuozhou@sensorsdata.cn>
2019-05-27 11:28:00 +0800

    集成极光

--- demo/demo/AppDelegate.m
+++ demo/demo/AppDelegate.m
@@ -9,7 +9,16 @@
 #import "AppDelegate.h"
 #import <SensorsSDK/SensorsSDK.h>
 
-@interface AppDelegate ()
+#import "JPUSHService.h"
+// iOS10 注册 APNs 所需头文件
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h>
+#endif
+
+//极光推送
+static NSString *const JPush_AppKey = @"7049fbac28974cd3b1faae99";
+
+@interface AppDelegate ()<JPUSHRegisterDelegate>
 
 @end
 
@@ -20,7 +29,108 @@
     // Override point for customization after application launch.
     
     [SensorsAnalyticsSDK sharedInstance];
+    
+    [self setupJPushNotification:launchOptions];
     return YES;
+}
+
+#pragma mark - 处理推送消息
+
+//初始化极光推送
+- (void)setupJPushNotification:(NSDictionary *)launchOptions {
+
+    JPUSHRegisterEntity * entity = [[JPUSHRegisterEntity alloc] init];
+    if (@available(iOS 12.0, *)) {
+        entity.types = JPAuthorizationOptionAlert|JPAuthorizationOptionBadge|JPAuthorizationOptionSound|JPAuthorizationOptionProvidesAppNotificationSettings;
+    } else {
+        // Fallback on earlier versions
+    }
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8.0) {
+        // 可以添加自定义 categories
+        // NSSet<UNNotificationCategory *> *categories for iOS10 or later
+        // NSSet<UIUserNotificationCategory *> *categories for iOS8 and iOS9
+    }
+    [JPUSHService registerForRemoteNotificationConfig:entity delegate:self];
+    
+    
+#ifdef DEBUG
+    [JPUSHService setupWithOption:launchOptions appKey:JPush_AppKey channel:@"App Store" apsForProduction:NO advertisingIdentifier:nil];
+#else
+    [JPUSHService setupWithOption:launchOptions appKey:JPush_AppKey channel:@"App Store" apsForProduction:YES advertisingIdentifier:nil];
+#endif
+}
+
+//注册 APNs 成功并上报 DeviceToken
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    /// Required - 注册 DeviceToken
+    [JPUSHService registerDeviceToken:deviceToken];
+}
+
+//实现注册 APNs 失败
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    //Optional
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+
+#pragma mark- JPUSHRegisterDelegate
+
+// iOS 12 Support
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center openSettingsForNotification:(UNNotification *)notification API_AVAILABLE(ios(10.0)) API_AVAILABLE(ios(10.0)){
+    if (@available(iOS 10.0, *)) {
+        if (notification && [notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+            //从通知界面直接进入应用
+        }else{
+            //从通知设置界面进入应用
+        }
+    } else {
+        // Fallback on earlier versions
+    }
+}
+
+// iOS 10 Support
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler  API_AVAILABLE(ios(10.0)) API_AVAILABLE(ios(10.0)){
+    // Required
+    NSDictionary * userInfo = notification.request.content.userInfo;
+    if (@available(iOS 10.0, *)) {
+        if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+            [JPUSHService handleRemoteNotification:userInfo];
+        }
+    } else {
+        // Fallback on earlier versions
+    }
+    if (@available(iOS 10.0, *)) {
+        completionHandler(UNNotificationPresentationOptionAlert);
+    } else {
+        // Fallback on earlier versions
+    } // 需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
+}
+
+// iOS 10 Support
+- (void)jpushNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler  API_AVAILABLE(ios(10.0)) API_AVAILABLE(ios(10.0)){
+    // Required
+    NSDictionary * userInfo = response.notification.request.content.userInfo;
+    if (@available(iOS 10.0, *)) {
+        if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+            [JPUSHService handleRemoteNotification:userInfo];
+        }
+    } else {
+        // Fallback on earlier versions
+    }
+    completionHandler();  // 系统要求执行这个方法
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    
+    // Required, iOS 7 Support
+    [JPUSHService handleRemoteNotification:userInfo];
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+    // Required, For systems with less than or equal to iOS 6
+    [JPUSHService handleRemoteNotification:userInfo];
 }
 
 
