42d4b90cc583ae70c850c32c263080d113330a28
张敏超🍎 <zhangminchao@sensorsdata.cn>
2019-09-08 19:54:49 +0800

    创建网络请求类

---
+++ SensorsSDK/Core/SensorsAnalyticsNetwork.h
@@ -0,0 +1,17 @@
+//
+//  SensorsAnalyticsNetwork.h
+//  SensorsSDK
+//
+//  Created by 张敏超🍎 on 2019/9/8.
+//  Copyright © 2019 王灼洲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SensorsAnalyticsNetwork : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SensorsSDK/Core/SensorsAnalyticsNetwork.m
@@ -0,0 +1,21 @@
+//
+//  SensorsAnalyticsNetwork.m
+//  SensorsSDK
+//
+//  Created by 张敏超🍎 on 2019/9/8.
+//  Copyright © 2019 王灼洲. All rights reserved.
+//
+
+#import "SensorsAnalyticsNetwork.h"
+
+@implementation SensorsAnalyticsNetwork
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
